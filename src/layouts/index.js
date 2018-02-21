d42699b2c65dd24066ffad327c1296d8ca8d2de4
Panu Tangchalermkul <panuta@gmail.com>
2018-02-21 15:48:15 +0700

    Set link for current page active

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'gatsby-link';
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
@@ -6,17 +7,17 @@ import '../../scss/styles.scss';
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet  title="Panu Tangchalermkul">
-      <html lang="en" />
-      <meta charSet="utf-8" />
+    <Helmet  title='Panu Tangchalermkul'>
+      <html lang='en' />
+      <meta charSet='utf-8' />
     </Helmet>
-    <nav className={'navbar'}>
-      <ul className={'navbar-nav'}>
-        <li className={'nav-item active'}>
-          <a className={'nav-link'} href="#">HOME</a>
+    <nav className='navbar'>
+      <ul className='navbar-nav'>
+        <li className='nav-item'>
+          <Link className='nav-link' to='/' activeClassName='active' exact={true}>HOME</Link>
         </li>
-        <li className={'nav-item'}>
-          <a className={'nav-link'} href="#">CONTACT</a>
+        <li className='nav-item'>
+          <Link className='nav-link' to='/contact/' activeClassName='active'>CONTACT</Link>
         </li>
       </ul>
 
